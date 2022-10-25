ea696fd84601794d60a94653570cbbc68ab571b8
vladovichsvetlana <107796276+vladovichsvetlana@users.noreply.github.com>
2022-10-25 21:40:49 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+roll_dice = input("Write start to dice roll: ")
+
+if roll_dice == "start":
+   posiblle_results = [1, 2, 3, 4, 5, 6]
+   result = random.choice(posiblle_results)
+   print("Result of dice rolling is : " + str(result))
