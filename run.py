0afac0c04a9a66dfe8220ed5efc4cc6c95b96f79
vladovichsvetlana <vladovichsvetlana@gmail.com>
2022-10-25 21:01:47 +0000

    adding dicebreaker questions

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ import random
 roll_dice = input("Write start to dice roll: ")
 
 if roll_dice == "start":
-   posiblle_results = [1, 2, 3, 4, 5, 6]
-   result = random.choice(posiblle_results)
+   possible_results = [1, 2, 3, 4, 5, 6]
+   result = random.choice(possible_results)
    print("Result of dice rolling is : " + str(result))
