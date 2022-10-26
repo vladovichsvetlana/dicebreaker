bc4b5b47f665d799fd0e39443b9540c53314c2ab
vladovichsvetlana <107796276+vladovichsvetlana@users.noreply.github.com>
2022-10-26 22:38:12 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,29 +1,14 @@
-import random
-roll_dice = input("Write start to dice roll: ")
+import random 
 
-if roll_dice == "start":
-   possible_results = [1, 2, 3, 4, 5, 6]
-   result = random.choice(possible_results)
-   print("Result of dice rolling is : " + str(result))
+questions = ["Q 1 chosen: What was one fun thing you did this summer?", "Q 2 chosen: What are your two favourite colours?", "Q 3 chosen: List three words that best describe you?", "Q 4 chosen: Name four of your favourite movies", "Q 5 chosen: List five of your favourite foods", "Q 6 chosen: Name six places you want to visit in your life"]
+play = 0
+while play != "q":
+    for x in range(1, 7):
+        roll_dice = input("Press any key to roll dice:")
+    result = random.randrange(1, 6)
+    print(questions[result])
+    x += 1
 
-if result == '1':
-   print("What was one fun thing you did this summer?")
+    play = input("Press any key to play again or q to quit")
 
-elif result == "2":
-   print("What are your two favourite colours?")
-
-elif result == "3":
-   print ("List three words that best describe you?")
-
-elif result == "4":
-   print ("Name four of your favourite movies")
-
-elif result == "5":
-   print ("List five of your favourite foods")
-
-elif result == "6":
-   print ("Name six places you want to visit in your life")
-
-else:
-print('Thanks For Playing')
-break
+print("Good Bye. Thank you for playing")
