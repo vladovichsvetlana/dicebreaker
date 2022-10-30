916c271a6d642f6188db73d3c60c3e15aa9d6193
vladovichsvetlana <107796276+vladovichsvetlana@users.noreply.github.com>
2022-10-30 15:30:21 +0000

    Update run.py
    splitting questions into 6 lines to fix the error having the line not longer than 79 using phyton code formatter

--- run.py
+++ run.py
@@ -1,13 +1,20 @@
 import random 
 
-questions = ["Q 1 chosen: What was one fun thing you did this summer?", "Q 2 chosen: What are your two favourite colours?", "Q 3 chosen: List three words that best describe you?", "Q 4 chosen: Name four of your favourite movies", "Q 5 chosen: List five of your favourite foods", "Q 6 chosen: Name six places you want to visit in your life"]
+questions = [
+    "Q 1 chosen: What was one fun thing you did this summer?",
+    "Q 2 chosen: What are your two favourite colours?", 
+    "Q 3 chosen: List three words that best describe you?", 
+    "Q 4 chosen: Name four of your favourite movies", 
+    "Q 5 chosen: List five of your favourite foods", 
+    "Q 6 chosen: Name six places you want to visit in your life",
+]
 play = 0
 while play != "q":
     for x in range(1, 7):
         roll_dice = input("Press any key to roll dice:")
-    result = random.randrange(1, 6)
-    print(questions[result])
-    x += 1
+        result = random.randrange(1, 6)
+        print(questions[result])
+        x += 1
 
     play = input("Press any key to play again or q to quit")
 
