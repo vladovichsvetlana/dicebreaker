8b355aecdc45c2654d180927bb03c1ff6afdbc9f
vladovichsvetlana <107796276+vladovichsvetlana@users.noreply.github.com>
2022-10-31 09:01:08 +0000

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,7 @@
+# pylint: disable=missing-module-docstring\n
+# pylint: disable=missing-class-docstring\n
+# pylint: disable=missing-function-docstring\n
+
 # Import required modules
 import random
 import sys
@@ -16,11 +20,33 @@ else:
         3. If dice has number 1, question 1 is to be answered and so on.\n'
         4. Game can be restarted after 6 attempts or stopped''')
 
+# starting the scores with zero
+player1_score = 0
+player2_score = 0
+
+# Generating random values for both players
+player1_value = random.randint(1, 6)
+player2_value = random.randint(1, 6)
+
+# Stating the players
+print("Player 1 rolled: ", player1_value)
+print("Player 2 rolled: ", player2_value)
+
+# Comparison of values
+if player1_value > player2_value:
+    print("player 1 wins.")
+    player1_score = player1_score + 1  
+elif player2_value > player1_value:
+    print("player 2 wins")
+    player2_score = player2_score + 1
+else:
+    print("It's a draw")
+
 # To introduce the dicebreaker questions
 questions = [
     "Q 1 chosen: What was one fun thing you did this summer?",
     "Q 2 chosen: What are your two favourite colours?", 
-    "Q 3 chosen: List three words that best describe you?",  
+    "Q 3 chosen: List three words that best describe you?",
     "Q 4 chosen: Name four of your favourite movies", 
     "Q 5 chosen: List five of your favourite foods", 
     "Q 6 chosen: Name six places you want to visit in your life",
