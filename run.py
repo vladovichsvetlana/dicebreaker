9bf52bc5d2077a42544794847515db015ef444c0
vladovichsvetlana <107796276+vladovichsvetlana@users.noreply.github.com>
2022-11-01 23:36:54 +0000

    Update run.py
    adding dice face art

--- run.py
+++ run.py
@@ -10,7 +10,7 @@ import sys
 print("Hello everyone! Welcome to the dicebreaker game!")
 
 # Start the game
-a = input("To start the game type yes and to quit type no")
+a = input("To start the game type yes and to quit type no:")
 if a.lower() == "no":
     sys.exit()
 else:
@@ -41,6 +41,78 @@ elif player2_value > player1_value:
     player2_score = player2_score + 1
 else:
     print("It's a draw")
+
+# Dice images
+DICE_ART = {
+	    1: (
+	        "┌─────────┐",
+	        "│         │",
+	        "│    ●    │",
+	        "│         │",
+	        "└─────────┘",
+	    ),
+	    2: (
+	        "┌─────────┐",
+	        "│  ●      │",
+	        "│         │",
+	        "│      ●  │",
+	        "└─────────┘",
+	    ),
+	    3: (
+	        "┌─────────┐",
+	        "│  ●      │",
+	        "│    ●    │",
+	        "│      ●  │",
+	        "└─────────┘",
+	    ),
+	    4: (
+	        "┌─────────┐",
+	        "│  ●   ●  │",
+	        "│         │",
+	        "│  ●   ●  │",
+	        "└─────────┘",
+	    ),
+	    5: (
+	        "┌─────────┐",
+	        "│  ●   ●  │",
+	        "│    ●    │",
+	        "│  ●   ●  │",
+	        "└─────────┘",
+	    ),
+	    6: (
+	        "┌─────────┐",
+	        "│  ●   ●  │",
+	        "│  ●   ●  │",
+	        "│  ●   ●  │",
+	        "└─────────┘",
+	    ),
+	}
+
+DIE_HEIGHT = len(DICE_ART[1])
+DIE_WIDTH = len(DICE_ART[1][0])
+DIE_FACE_SEPARATOR = " "
+
+
+def parse_input(input_string):
+    if input_string.strip() in {"1", "2", "3", "4", "5", "6"}:
+        return int(input_string)
+    else:
+        print("Please enter a number from 1 to 6.")
+        raise SystemExit(1)
+
+# Return diagram of dice faces from dice_values
+
+
+def generate_dice_faces_diagram(dice_values):
+    dice_faces = _get_dice_faces(dice_values)
+    
+
+def _get_dice_faces(dice_values):
+    dice_faces = []
+    for value in dice_values:
+        dice_faces.append(DICE_ART[value])
+    return dice_faces
+print(f"\n{dice_face_diagram}") # Display the diagram
 
 # To introduce the dicebreaker questions
 questions = [
